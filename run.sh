b4d553c9cb119d285fecb9eabfb8d8d1f888b373
Oshawk <oshawk@protonmail.com>
2026-04-18 15:19:50 +0100

    Add password authentication

--- run.sh
+++ run.sh
@@ -10,11 +10,13 @@ cd grader
 sudo docker build -t assembly-grader . || exit 1
 cd ..
 
+docker build -t prairielearn-password-authentication 'https://github.com/Oshawk/PrairieLearn.git#password_authentication'
+
 sudo docker run -it --rm -p 3000:3000 \
     -v "$WD/course:/course" \
     -v "$WD/.runtime/jobs:/jobs" \
     -e HOST_JOBS_DIR="$WD/.runtime/jobs" \
     -v /var/run/docker.sock:/var/run/docker.sock \
     --add-host=host.docker.internal:172.17.0.1 \
-    prairielearn/prairielearn \
+    prairielearn-password-authentication \
     bash -c 'rm -r /PrairieLearn/*Course && /PrairieLearn/scripts/init.sh'
